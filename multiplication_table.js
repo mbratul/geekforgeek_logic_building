285594a5affc309ef8b536b2e09131fc34283008
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-05-04 22:21:52 +0600

    update example for better understanding

--- multiplication_table.js
+++ multiplication_table.js
@@ -3,6 +3,21 @@
 
     Problem: Given a number n, we need to print its table. 
     https://www.geeksforgeeks.org/program-to-print-multiplication-table-of-a-number/
+
+    Examples : 
+    
+    Input:  5
+    Output: 
+    5 * 1 = 5
+    5 * 2 = 10
+    5 * 3 = 15
+    5 * 4 = 20
+    5 * 5 = 25
+    5 * 6 = 30
+    5 * 7 = 35
+    5 * 8 = 40
+    5 * 9 = 45
+    5 * 10 = 50
  */
 
 // Javascript program to print
