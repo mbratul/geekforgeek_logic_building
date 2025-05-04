9db44a15b03627ba07f0767dfadb87f1da0c72a4
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-05-04 21:26:47 +0600

    sum of natural number

---
+++ sum_of_naturals.js
@@ -0,0 +1,23 @@
+/* 
+    Program to find sum of first n natural numbers
+
+    Problem : Given a number n, find the sum of the first n natural numbers. 
+    https://www.geeksforgeeks.org/program-find-sum-first-n-natural-numbers/
+ */
+
+// Javascript program to find sum of first n natural numbers.
+
+function find_sum(n) {
+  let sum = 0;
+  for (let x = 1; x <= n; x++) {
+    sum = sum + x;
+  }
+  return sum;
+}
+let n = 3;
+console.log(find_sum(n));
+
+/* 
+    Solution: [Naive Approach] Loop Based Summation
+    Calculate the sum of all integers from 1 to n by iterating through a loop.
+ */
