2b69737e7c39cf82faae29dceba61c0dddab9f20
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-12-17 13:51:08 +0600

    sum of square natural numbers

---
+++ sum_of_squares_of_naturals.js
@@ -0,0 +1,20 @@
+{
+  /**
+   * Program for Sum of squares of first n natural numbers
+   * Given a positive integer n, we have to find the sum of squares of first n natural numbers.
+   * Examples :
+   * Input : n = 2
+   * Output: 5
+   * Explanation: 1^2+2^2 = 5
+   */
+
+  function summation(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i * i;
+    }
+    return sum;
+  }
+  let n = 2;
+  console.log(summation(8));
+}
